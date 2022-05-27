cad5f24e896089249a2c9da5f34e9291be6ea56c
Jorge A. Benavides <jorge.benavides@wizeline.com>
2022-05-27 17:58:26 -0500

    receiving args in script

--- generate.swift
+++ generate.swift
@@ -16,58 +16,60 @@ extension URL {
 
 let ROOT = URL(string: FileManager.default.currentDirectoryPath)!
 
-let folderName = "pokedex"
+CommandLine.arguments.forEach({ folderName in
 
-let folder = ROOT.appendingPathComponent(folderName)
+    let folder = ROOT.appendingPathComponent(folderName)
 
-folder.contentsOfDirectory.forEach { sub in
-    let subfolder = folder.appendingPathComponent(sub)
-    let filenames = subfolder.contentsOfDirectory.filter({ !$0.starts(with: ".") })
+    folder.contentsOfDirectory.forEach { sub in
+        let subfolder = folder.appendingPathComponent(sub)
+        let filenames = subfolder.contentsOfDirectory.filter({ !$0.starts(with: ".") })
 
-    filenames.forEach { filename in
-        guard let string = try? String(contentsOfFile: subfolder.appendingPathComponent(filename).absoluteString, encoding: .utf8),
-              let data = string.data(using: .utf8, allowLossyConversion: true),
-              let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [NSDictionary]
-        else { return }
-        let basename = (filename as NSString).deletingPathExtension
-        json.forEach { entry in
-            guard let id = entry["id"] as? String else { return }
-            let entryFolder = subfolder.appendingPathComponent(basename)
-            let entryFile = entryFolder.appendingPathComponent("\(id).json")
+        filenames.forEach { filename in
+            guard let string = try? String(contentsOfFile: subfolder.appendingPathComponent(filename).absoluteString, encoding: .utf8),
+                  let data = string.data(using: .utf8, allowLossyConversion: true),
+                  let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [NSDictionary]
+            else { return }
+            let basename = (filename as NSString).deletingPathExtension
+            json.forEach { entry in
+                guard let id = entry["id"] as? String else { return }
+                let entryFolder = subfolder.appendingPathComponent(basename)
+                let entryFile = entryFolder.appendingPathComponent("\(id).json")
+                do {
+                    try FileManager.default.createDirectory(atPath: entryFolder.absoluteString, withIntermediateDirectories: true)
+                    try JSONSerialization
+                        .data(withJSONObject: entry, options: [.prettyPrinted, .withoutEscapingSlashes])
+                        .write(to: URL(fileURLWithPath: entryFile.absoluteString))
+                } catch {
+                    print(error)
+                }
+            }
+            let schema: NSMutableDictionary = NSMutableDictionary(dictionary: json.first ?? [:])
+            let schemaFile = subfolder.appendingPathComponent(".schema.json")
+            schema.allKeys.forEach({ key in
+                guard let key = key as? String else { return }
+                let value = schema[key]
+                if ((value as? Bool) != nil) {
+                    schema.setValue("boolean", forKey: key)
+                } else if ((value as? Int) != nil) {
+                    schema.setValue("integer", forKey: key)
+                } else if ((value as? Double) != nil) {
+                    schema.setValue("double", forKey: key)
+                } else if ((value as? String) != nil) {
+                    schema.setValue("string", forKey: key)
+                } else if ((value as? [Any]) != nil) {
+                    schema.setValue("array", forKey: key)
+                } else {
+                    schema.setValue("unknown", forKey: key)
+                }
+            })
             do {
-                try FileManager.default.createDirectory(atPath: entryFolder.absoluteString, withIntermediateDirectories: true)
                 try JSONSerialization
-                    .data(withJSONObject: entry, options: [.prettyPrinted, .withoutEscapingSlashes])
-                    .write(to: URL(fileURLWithPath: entryFile.absoluteString))
+                    .data(withJSONObject: schema, options: .prettyPrinted)
+                    .write(to: URL(fileURLWithPath: schemaFile.absoluteString))
             } catch {
                 print(error)
             }
         }
-        let schema: NSMutableDictionary = NSMutableDictionary(dictionary: json.first ?? [:])
-        let schemaFile = subfolder.appendingPathComponent(".schema.json")
-        schema.allKeys.forEach({ key in
-            guard let key = key as? String else { return }
-            let value = schema[key]
-            if ((value as? Bool) != nil) {
-                schema.setValue("boolean", forKey: key)
-            } else if ((value as? Int) != nil) {
-                schema.setValue("integer", forKey: key)
-            } else if ((value as? Double) != nil) {
-                schema.setValue("double", forKey: key)
-            } else if ((value as? String) != nil) {
-                schema.setValue("string", forKey: key)
-            } else if ((value as? [Any]) != nil) {
-                schema.setValue("array", forKey: key)
-            } else {
-                schema.setValue("unknown", forKey: key)
-            }
-        })
-        do {
-            try JSONSerialization
-                .data(withJSONObject: schema, options: .prettyPrinted)
-                .write(to: URL(fileURLWithPath: schemaFile.absoluteString))
-        } catch {
-            print(error)
-        }
     }
-}
+
+})
